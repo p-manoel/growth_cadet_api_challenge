8d33e1b8bc3f524d0c63c76869fc332fd3a4b045
Pedro Manoel <dev.pedromanoel@pm.me>
2024-01-09 02:43:14 -0300

    Add dns_records#index action

--- app/controllers/api/v1/dns_records_controller.rb
+++ app/controllers/api/v1/dns_records_controller.rb
@@ -3,14 +3,22 @@ module Api
     class DnsRecordsController < ApplicationController
       # GET /dns_records
       def index
-        # TODO: Implement this action
+        input = {
+          page: index_dns_records_params[:page]&.to_i,
+          included: index_dns_records_params[:included].presence&.split(',') || [],
+          excluded: index_dns_records_params[:excluded].presence&.split(',') || []
+        }
+
+        ::Dns::Fetch.call(input)
+          .on_success { render json: _1[:formatted_response], status: :ok }
+          .on_failure { render json: { errors: _1[:errors] }, status: :unprocessable_entity }
       end
 
       # POST /dns_records
       def create
         input = {
-          ip: dns_record_params[:ip].presence,
-          hostnames: dns_record_params[:hostnames_attributes]&.map { _1[:hostname] }
+          ip: create_dns_record_params[:ip].presence,
+          hostnames: create_dns_record_params[:hostnames_attributes]&.map { _1[:hostname] }
         }
 
         ::Dns::Create.call(input)
@@ -21,11 +29,16 @@ module Api
 
       private
 
-      def dns_record_params
-        params.require(:dns_record).permit(
+      def create_dns_record_params
+        params.require(:dns_record
+        ).permit(
           :ip,
           hostnames_attributes: [:hostname]
         )
+      end
+
+      def index_dns_records_params
+        params.permit(:page, :included, :excluded)
       end
     end
   end
